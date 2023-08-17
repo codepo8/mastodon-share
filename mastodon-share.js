057208bc6da45a4d859756b6d9d7cd68166b5933
Christian Heilmann <chris.heilmann@gmail.com>
2023-08-18 00:04:38 +0200

    adding comments

--- mastodon-share.js
+++ mastodon-share.js
@@ -1,7 +1,15 @@
+// Grab link from the DOM
 const button = document.querySelector('.mastodon-share');
+
+// When a user clicks the link
 button.addEventListener('click', (e) => {
+
+// If the user has already entered their instance and it is in localstorage
+// write out the link href with the instance and the current page title and URL
 if(localStorage.getItem('mastodon-instance')) {
   button.href = `https://${localStorage.getItem('mastodon-instance')}/share?text=${encodeURIComponent(document.title)}%0A${encodeURIComponent(location.href)}`;
+
+// otherwise, prompt the user for their instance and save it to localstorage
 } else {
     e.preventDefault();
     let instance = window.prompt('Please tell me your Mastodon instance');
