606fb8bf81d292529a8d9e3578901a5071c3614e
Christian Heilmann <chris.heilmann@gmail.com>
2023-08-18 11:57:36 +0200

    Adding PHP version with extra span

---
+++ sharebutton.php
@@ -0,0 +1,9 @@
+<p class="mastodonshare">
+  <a class="tootshare" 
+     href="https://mastodon.social/share?text=<?php 
+            echo get_the_title()).' '.
+            urlencode(the_permalink());
+     ?>" target="blank" rel="noreferrer noopener">
+       Share on Mastodon <span>(needs instance)</span>
+  </a>
+</p>
