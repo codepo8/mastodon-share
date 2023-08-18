909dd16dbb2f31faf65b7348293ef426dda01ad3
Christian Heilmann <chris.heilmann@gmail.com>
2023-08-18 12:08:48 +0200

    Oh noes, PHP error

--- sharebutton.php
+++ sharebutton.php
@@ -1,7 +1,7 @@
 <p class="mastodonshare">
   <a class="tootshare" 
      href="https://mastodon.social/share?text=<?php 
-            echo get_the_title()).' '.
+            echo get_the_title().' '.
             urlencode(the_permalink());
      ?>" target="blank" rel="noreferrer noopener">
        Share on Mastodon <span>(needs instance)</span>
