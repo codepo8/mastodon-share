466e5f013a4d2f28e8e980fcda2f92c9d86753b6
Christian Heilmann <chris.heilmann@gmail.com>
2023-08-18 19:34:38 +0200

    Removed PHP version, not needed.

--- sharebutton.php
+++
@@ -1,9 +0,0 @@
-<p class="mastodonshare">
-  <a class="tootshare" 
-     href="https://mastodon.social/share?text=<?php 
-            echo get_the_title().' '.
-            urlencode(the_permalink());
-     ?>" target="blank" rel="noreferrer noopener">
-       Share on Mastodon <span>(needs instance)</span>
-  </a>
-</p>
