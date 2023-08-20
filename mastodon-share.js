fccb798a133ac70ef0e0abf4b9e5ef52eab94065
Christian Heilmann <chris.heilmann@gmail.com>
2023-08-20 21:41:23 +0200

    Preset the prompt and fixed the button display

--- mastodon-share.js
+++ mastodon-share.js
@@ -1,21 +1,21 @@
 
 // I got the key, I got the secret…
 let key = 'mastodon-instance';
+let instance = localStorage.getItem(key);
 
 // get the link from the DOM
 const button = document.querySelector('.mastodon-share');
 
 // got it? Let's go! 
 if (button) {
-
     // labels and texts from the link
     let prompt = button.dataset.prompt || 'Please tell me your Mastodon instance';
     let editlabel = button.dataset.editlabel || 'Edit your Mastodon instance';
     let edittext = button.dataset.edittext || '✏️';
-    
+
     // Ask the user for the instance name and set it…
     const setinstance = _ => {
-        let instance = window.prompt(prompt);
+        instance = window.prompt(prompt, instance);
         if(instance) {
             localStorage.setItem(key, instance);
             createeditbutton();
@@ -26,7 +26,7 @@ if (button) {
     const createeditbutton = _ => {
         if (document.querySelector('button.mastodon-edit')) return;
         let editlink = document.createElement('button');
-          editlink.innerText = '✏️';
+          editlink.innerText = edittext;
           editlink.classList.add('mastodon-edit');
           editlink.title = editlabel;
           editlink.ariaLabel = editlabel;
